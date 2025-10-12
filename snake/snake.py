aa36f24e2676700aef4bdef9d9caedce8ee92141
figdavi <figueiredodavi.dev@proton.me>
2025-10-12 18:02:24 -0300

    Etapa 1 (ref)

--- snake/snake.py
+++ snake/snake.py
@@ -1,3 +1,3 @@
 class Snake:
-    def __init__(self, position):
-        self.position = position
+    def __init__(self, start_pos: tuple[int, int]):
+        self.position = start_pos
--- snake/testes.py
+++ snake/testes.py
@@ -2,7 +2,10 @@ import pytest
 from .snake import Snake
 
 
-def test_constructor():
-    snake = Snake(position=(0, 0))
+@pytest.fixture
+def snake():
+    return Snake(start_pos=(0, 0))
 
+
+def test_constructor(snake: Snake):
     assert snake.position == (0, 0)
