085693877d10a364c3c4c24334bbba8133035bba
figdavi <figueiredodavi.dev@proton.me>
2025-10-12 20:02:49 -0300

    Etapa 2 (red)
    Test snake moves in a direction

--- snake/testes.py
+++ snake/testes.py
@@ -10,3 +10,9 @@ def snake():
 def test_constructor(snake: Snake):
     assert snake.head == (0, 0)
     assert snake.body == [(0, 0)]
+
+
+def test_snake_moves_right(snake: Snake):
+    snake.move("right")
+    assert snake.head == (1, 0)
+    assert snake.body == [(1, 0)]
