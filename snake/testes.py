cbe77e2e970518024ce2ab32f7b93bbfbc58944c
figdavi <figueiredodavi.dev@proton.me>
2025-10-12 23:02:24 -0300

    Etapa 10 (ref 2)

--- snake/testes.py
+++ snake/testes.py
@@ -7,7 +7,7 @@ def snake():
     return Snake(head=(0, 0), body=[(0, 0)])
 
 
-def test_constructor(snake: Snake):
+def test_snake_constructor(snake: Snake):
     assert snake.head == (0, 0)
     assert snake.body == [(0, 0)]
     assert snake.direction == "up"
