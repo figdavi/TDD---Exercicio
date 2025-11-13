06824c4fcc88910c708ad1ae079d34d3b1d7d9b7
figdavi <figueiredodavi.dev@proton.me>
2025-11-13 02:07:59 -0300

    Scale drawings

--- snake/main.py
+++ snake/main.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from snake.snake import Direction
 from snake.game import Game
 
-# Get graphics folder path
-GRAPHICS_DIR = Path(__file__).resolve().parent / "graphics"
-
 # Constants
-DIMENSIONS = (640, 640)
+CELL_SIZE = 20
+GRID_WIDTH = 640 // CELL_SIZE
+GRID_HEIGHT = 640 // CELL_SIZE
+DIMENSIONS = (GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE)
 dir_map: dict[int, Direction] = {
     pygame.K_w: "up",
     pygame.K_s: "down",
@@ -16,12 +16,13 @@ dir_map: dict[int, Direction] = {
 }
 
 # Game initilization
-game = Game(DIMENSIONS[0], DIMENSIONS[1])
+game = Game(GRID_HEIGHT, GRID_WIDTH)
 pygame.init()
 screen = pygame.display.set_mode((DIMENSIONS[0], DIMENSIONS[1]))
 clock = pygame.time.Clock()
 
 # Sprites
+GRAPHICS_DIR = Path(__file__).resolve().parent / "graphics"
 snake_head_img = pygame.image.load(GRAPHICS_DIR / "head_up.png").convert_alpha()
 snake_body_img = pygame.image.load(GRAPHICS_DIR / "body_vertical.png").convert_alpha()
 fruit_img = pygame.image.load(GRAPHICS_DIR / "apple.png").convert_alpha()
@@ -47,16 +48,17 @@ while running:
     game.spawn_fruits()
 
     # Snake drawing
-    screen.blit(snake_head_img, game.snake.head)
+    screen.blit(
+        snake_head_img, (game.snake.head[0] * CELL_SIZE, game.snake.head[1] * CELL_SIZE)
+    )
     for x, y in game.snake.body:
-        screen.blit(snake_body_img, (x, y))
+        screen.blit(snake_body_img, (x * CELL_SIZE, y * CELL_SIZE))
 
     # Fruit drawing
     for x, y in game.fruits:
-        screen.blit(fruit_img, (x, y))
+        screen.blit(fruit_img, (x * CELL_SIZE, y * CELL_SIZE))
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(10)
 
-print("Quitting..")
 pygame.quit()
