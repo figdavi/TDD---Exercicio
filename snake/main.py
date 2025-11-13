2d02b77fb9b8361ce1df22e6f9ebfe18fafc9004
figdavi <figueiredodavi.dev@proton.me>
2025-11-13 01:43:47 -0300

    Add snake drawing logic

--- snake/main.py
+++ snake/main.py
@@ -1,8 +1,12 @@
 import pygame
+from pathlib import Path
 from snake.snake import Direction
 from snake.game import Game
 
-DIMENSIONS = (10, 15)
+# Criar o caminho do arquivo de input e output
+GRAPHICS_DIR = Path(__file__).resolve().parent / "graphics"
+
+DIMENSIONS = (640, 640)
 dir_map: dict[int, Direction] = {
     pygame.K_w: "up",
     pygame.K_s: "down",
@@ -14,8 +18,12 @@ game = Game(DIMENSIONS[0], DIMENSIONS[1])
 pygame.init()
 screen = pygame.display.set_mode((DIMENSIONS[0], DIMENSIONS[1]))
 
+snake_head_img = pygame.image.load(GRAPHICS_DIR / "head_up.png").convert()
+snake_body_img = pygame.image.load(GRAPHICS_DIR / "body_vertical.png").convert()
+
 running = True
 while running:
+    # Quit button and key pressing logic
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -24,6 +32,14 @@ while running:
                 running = False
             elif event.key in dir_map:
                 game.snake.change_direction(new_direction=dir_map[event.key])
+
+    # Snake drawing
+    screen.blit(snake_head_img, game.snake.head)
+
+    for x, y in game.snake.body:
+        screen.blit(snake_body_img, (x, y))
+
+    pygame.display.flip()
 
 print("Quitting..")
 pygame.quit()
